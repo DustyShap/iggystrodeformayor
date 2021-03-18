15de7e1229118f316fa988657cb2555858ccbaa6
Dustin Shapiro <dustinshapiro@gmail.com>
2021-03-18 00:23:26 -0400

    adding issues

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/issues')
+def issues():
+    return render_template('issues.html')
